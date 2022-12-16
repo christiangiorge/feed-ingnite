7677d2dbbfd85cb3a2b85af0b61c97b3710744cd
Christian Giorge <chrisgiorgeas@gmail.com>
2022-12-16 06:25:52 -0300

    Desenvolvendo o sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
 
 import './global.css'
 
 function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          lapsodkpaskdp[asokv o[sodj[sovj s[omas[dkas[ok[ pok[psaok pok[ pok [pokas[doks[pdok]]]]]]]]]]]]
+
+        </main>
+      </div>
+    </div>
+    
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1542372147193-a7aca54189cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50"/>
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/christiangiorge.png"/>
+
+                <strong>Christian Giorge</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
