ac663f22ee127b40e128f0a0a4dc46ef6d39bbb2
Christian Giorge <chrisgiorgeas@gmail.com>
2022-12-19 06:25:06 -0300

    Melhorando o component Avatar e aplicando responsividade

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -8,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1542372147193-a7aca54189cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50"/>
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/christiangiorge.png"/>
+                <Avatar src="https://github.com/christiangiorge.png"/>
 
                 <strong>Christian Giorge</strong>
                 <span>Web Developer</span>
