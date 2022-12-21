44a605dcd2614f0581f0eab29ca2f4bff6b9452a
Christian Giorge <chrisgiorgeas@gmail.com>
2022-12-21 06:28:26 -0300

    Convertendo dados estáticos para dados dinâmicos.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/christiangiorge.png',
+      name: 'Christian Giorge',
+      role: 'CEO @CG Dev'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publisehdAt: new Date('2022-12-21 05:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publisehdAt: new Date('2022-12-20 18:20:00'),
+  }
+]
+
+
 function App() {
 
   return (
@@ -15,7 +47,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publisehdAt={post.publisehdAt}
+              />
+            )
+          })}
 
         </main>
       </div>
